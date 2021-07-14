a1f68f1142b976001d931805fb46f86e318191c5
harsha <munna.mar11@gmail.com>
2021-07-14 15:28:01 +0530

    settings change

--- src/settings.js
+++ src/settings.js
@@ -8,8 +8,8 @@ const boidTypes = {
     red: {
         color: '#d33021',
         tailColor: '#d3302166',
-        speedLimit: 5,
-        count: 20,
+        speedLimit: 3,
+        count: 40,
         centeringFactor: 0.005,
         matchingFactor: 0.05,
         avoidSelf: {
@@ -28,12 +28,12 @@ const boidTypes = {
     blue: {
         color: '#558cf4',
         tailColor: '#558cf466',
-        speedLimit: 10,
-        count: 10,
+        speedLimit: 7,
+        count: 20,
         centeringFactor: 0.005,
         matchingFactor: 0.05,
         avoidSelf: {
-            distance: 20,
+            distance: 100,
             factor: 0.05,
         },
         avoidOther: {
